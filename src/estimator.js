852908afca6dce01ce5326e74b685ec68783f56d
kolade Afeez <daisedalok@gmail.com>
2020-04-08 11:06:30 +0100

    test challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,24 @@
-const covid19ImpactEstimator = (data) => data;
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable linebreak-style */
 
+
+const covid19ImpactEstimator = (data) => {
+  const input = data;
+  const impactCurrentlyInfected = input.reportedCases * 10;
+  const severeCurrentlyInfected = input.reportedCases * 50;
+  const impactInfectionsByRequestedTime = impactCurrentlyInfected * (2 ** 9);
+  const severeInfectionsByRequestedTime = severeCurrentlyInfected * (2 ** 9);
+  return {
+    data: input, // the input data you got
+    impact: {
+      currentlyInfected: impactCurrentlyInfected,
+      infectionsByRequestedTime: impactInfectionsByRequestedTime
+    }, // your best case estimation
+    severeImpact: {
+      currentlyInfected: severeCurrentlyInfected,
+      infectionsByRequestedTime: severeInfectionsByRequestedTime
+    } // your severe case estimation
+  };
+};
 export default covid19ImpactEstimator;
