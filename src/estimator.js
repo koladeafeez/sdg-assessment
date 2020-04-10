57579565f7c6b876680ecdad6fcec75633a03e67
kolade Afeez <daisedalok@gmail.com>
2020-04-10 08:30:16 +0100

    refactor functions

--- src/estimator.js
+++ src/estimator.js
@@ -1,14 +1,47 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable no-shadow */
+/* eslint-disable linebreak-style */
 /* eslint-disable no-unused-vars */
 /* eslint-disable linebreak-style */
 
+
+// const data = {
+//   region: {
+//     name: 'Africa',
+//     avgAge: 19.7,
+//     avgDailyIncomeInUSD: 5,
+//     avgDailyIncomePopulation: 0.71
+//   },
+//   periodType: 'days',
+//   timeToElapse: 58,
+//   reportedCases: 674,
+//   population: 66622705,
+//   totalHospitalBeds: 1380614
+// };
+
+function covertToDays(timeFormat, timeToElapse) {
+  let elapseTime;
+  switch (timeFormat) {
+    case 'weeks':
+      elapseTime = timeToElapse * 7;
+      break;
+    case 'months':
+      elapseTime = timeToElapse * 30;
+      break;
+    default:
+      elapseTime = timeToElapse;
+  }
+  return elapseTime;
+}
 
 const covid19ImpactEstimator = (data) => {
   const input = data;
   const impactCurrentlyInfected = input.reportedCases * 10;
   const severeCurrentlyInfected = input.reportedCases * 50;
-  const impactInfectionsByRequestedTime = impactCurrentlyInfected * (2 ** 10);
-  const severeInfectionsByRequestedTime = severeCurrentlyInfected * (2 ** 10);
+  const periodToDays = covertToDays(input.periodType, input.timeToElapse);
+  const cycles = Math.floor(periodToDays / 3);
+  const impactInfectionsByRequestedTime = impactCurrentlyInfected * (2 ** cycles);
+  const severeInfectionsByRequestedTime = severeCurrentlyInfected * (2 ** cycles);
   return {
     data: input, // the input data you got
     impact: {
@@ -21,4 +54,6 @@ const covid19ImpactEstimator = (data) => {
     } // your severe case estimation
   };
 };
+
+
 export default covid19ImpactEstimator;
